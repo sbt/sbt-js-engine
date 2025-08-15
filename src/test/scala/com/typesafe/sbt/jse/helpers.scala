30b4caaa6365d2b1fd23e9e8f041a8f37b47ccda
Billy Autrey <40704452+BillyAutrey@users.noreply.github.com>
2025-08-15 17:19:14 -0500

    Formatting changes

--- src/test/scala/com/typesafe/sbt/jse/helpers.scala
+++ src/test/scala/com/typesafe/sbt/jse/helpers.scala
@@ -8,16 +8,16 @@ object helpers {
   // Define a custom equality function for LineBasedProblem
   def lineBasedProblemEquality(p1: LineBasedProblem, p2: LineBasedProblem): Boolean =
     p1.message == p2.message &&
-      p1.severity == p2.severity &&
-      p1.position.line == p2.position.line &&
-      p1.position.lineContent == p2.position.lineContent &&
-      p1.position.offset == p2.position.offset &&
-      p1.position.sourceFile.get.getCanonicalPath == p2.position.sourceFile.get.getCanonicalPath
+    p1.severity == p2.severity &&
+    p1.position.line == p2.position.line &&
+    p1.position.lineContent == p2.position.lineContent &&
+    p1.position.offset == p2.position.offset &&
+    p1.position.sourceFile.get.getCanonicalPath == p2.position.sourceFile.get.getCanonicalPath
 
   // Define a custom equality function for SourceResultPair
   def sourceResultPairEquality(p1: SourceResultPair, p2: SourceResultPair): Boolean =
     p1.result == p2.result &&
-      p1.source.getCanonicalPath == p2.source.getCanonicalPath
+    p1.source.getCanonicalPath == p2.source.getCanonicalPath
 
   private def stringifyLineBasedProblem(p: LineBasedProblem): String =
     s"""|LineBasedProblem(
